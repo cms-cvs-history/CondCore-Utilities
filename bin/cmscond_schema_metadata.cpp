bed6877110236ee3676c5300363b01c6a17d8dcf
Giacomo Govi <sha1-c6008bb91ca6bbc745928cb6f23a999d03f434c3@cern.ch>
2009-09-23 13:20:38 +0000

    adapted to the new db connection classes

--- bin/cmscond_schema_metadata.cpp
+++ bin/cmscond_schema_metadata.cpp
@@ -1,130 +1,57 @@
-#include "FWCore/PluginManager/interface/PluginManager.h"
-#include "FWCore/PluginManager/interface/standard.h"
-#include "CondCore/DBCommon/interface/CoralTransaction.h"
-#include "CondCore/DBCommon/interface/Connection.h"
-#include "CondCore/DBCommon/interface/ConnectionConfiguration.h"
-#include "CondCore/DBCommon/interface/AuthenticationMethod.h"
-#include "CondCore/DBCommon/interface/SessionConfiguration.h"
-#include "CondCore/DBCommon/interface/MessageLevel.h"
-#include "CondCore/DBCommon/interface/DBSession.h"
+#include "CondCore/DBCommon/interface/DbSession.h"
+#include "CondCore/DBCommon/interface/DbScopedTransaction.h"
 #include "CondCore/DBCommon/interface/Exception.h"
 #include "CondCore/MetaDataService/interface/MetaDataSchemaUtility.h"
-#include "CondCore/Utilities/interface/CommonOptions.h"
-//#include <boost/program_options.hpp>
+#include "CondCore/Utilities/interface/Utilities.h"
 #include <iostream>
-int main( int argc, char** argv ){
-  edmplugin::PluginManager::configure(edmplugin::standard::config());
-  cond::CommonOptions myopt("cmscond_schema_metadata");
-  myopt.addConnect();
-  myopt.addAuthentication(true);
-  myopt.visibles().add_options()
-    ("create","create schema")
-    ("drop","drop metadata schema")
-    ;
-  myopt.description().add( myopt.visibles() );
-  boost::program_options::variables_map vm;
-  try{
-    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(myopt.description()).run(), vm);
-    boost::program_options::notify(vm);
-  }catch(const boost::program_options::error& er) {
-    std::cerr << er.what()<<std::endl;
-    return 1;
-  }
-  if (vm.count("help")) {
-    std::cout <<  myopt.visibles() <<std::endl;;
+
+namespace cond {
+  class SchemaMetadataUtilities : public Utilities {
+    public:
+      SchemaMetadataUtilities();
+      ~SchemaMetadataUtilities();
+      int execute();
+  };
+}
+
+cond::SchemaMetadataUtilities::SchemaMetadataUtilities():Utilities("cmscond_schema_metadata"){
+  addConnectOption();
+  addAuthenticationOptions();
+  addOption<bool>("create","","create metadata schema");
+  addOption<bool>("drop","","drop metadata schema");
+}
+
+cond::SchemaMetadataUtilities::~SchemaMetadataUtilities(){
+}
+
+int cond::SchemaMetadataUtilities::execute(){
+
+  bool dropSchema= hasOptionValue("drop");
+  bool createSchema= hasOptionValue("create");
+  std::string tag;
+  cond::DbSession session = openDbSession("connect");
+  if( createSchema ){
+    cond::DbScopedTransaction transaction(session);
+    transaction.start(false);
+    cond::MetaDataSchemaUtility ut( session );
+    ut.create();
+    transaction.commit();
     return 0;
   }
-  std::string connect;
-  std::string authPath("");
-  std::string user("");
-  std::string pass("");
-  bool debug=false;
-  bool dropSchema=false;
-  bool createSchema=false;
-  std::string tag;
-  if(!vm.count("connect")){
-    std::cerr <<"[Error] no connect[c] option given \n";
-    std::cerr<<" please do "<<argv[0]<<" --help \n";
-    return 1;
-  }else{
-    connect=vm["connect"].as<std::string>();
-  }
-  if(vm.count("user")){
-    user=vm["user"].as<std::string>();
-  }
-  if(vm.count("pass")){
-    pass=vm["pass"].as<std::string>();
-  }
-  if( vm.count("authPath") ){
-    authPath=vm["authPath"].as<std::string>();
-  }
-  if(vm.count("create")){
-    createSchema=true;
-  }
-  if(vm.count("drop")){
-    dropSchema=true;
-  }
-  if(vm.count("debug")){
-    debug=true;
-  }
-  
-  cond::DBSession* session=new cond::DBSession;
-  session->configuration().connectionConfiguration()->disablePoolAutomaticCleanUp();
-  session->configuration().connectionConfiguration()->setConnectionTimeOut(0);
-
-  std::string userenv(std::string("CORAL_AUTH_USER=")+user);
-  std::string passenv(std::string("CORAL_AUTH_PASSWORD=")+pass);
-  ::putenv(const_cast<char*>(userenv.c_str()));
-  ::putenv(const_cast<char*>(passenv.c_str()));
-  if( !authPath.empty() ){
-    session->configuration().setAuthenticationMethod( cond::XML );
-    session->configuration().setAuthenticationPath(authPath);
-  }else{
-    session->configuration().setAuthenticationMethod( cond::Env );    
-  }
-  if( !authPath.empty() ){
-    session->configuration().setAuthenticationMethod( cond::XML );
-    session->configuration().setAuthenticationPath(authPath);
-  }else{
-    session->configuration().setAuthenticationMethod( cond::Env );
-  }
-  if(debug){
-    session->configuration().setMessageLevel( cond::Debug );
-  }else{
-    session->configuration().setMessageLevel( cond::Error );
-  }
-  cond::Connection con(connect,500);
-  session->open();
-  con.connect(session);
-  if( createSchema ){
-    try{
-      cond::CoralTransaction& coraldb=con.coralTransaction();
-      coraldb.start(false);
-      cond::MetaDataSchemaUtility ut(coraldb);
-      ut.create();
-      coraldb.commit();
-      return 0;
-    }catch(cond::Exception& er){
-      std::cout<<er.what()<<std::endl;
-    }catch(std::exception& er){
-      std::cout<<er.what()<<std::endl;
-    }
-  }
   if( dropSchema ){
-    try{
-      cond::CoralTransaction& coraldb=con.coralTransaction();
-      coraldb.start(false);
-      cond::MetaDataSchemaUtility ut(coraldb);
-      ut.drop();
-      coraldb.commit();      
-      return 0;
-    }catch(cond::Exception& er){
-      std::cout<<er.what()<<std::endl;
-    }catch(std::exception& er){
-      std::cout<<er.what()<<std::endl;
-    }
+    cond::DbScopedTransaction transaction(session);
+    transaction.start(false);
+    cond::MetaDataSchemaUtility ut( session );
+    ut.drop();
+    transaction.commit();
+    return 0;
   }
-  con.disconnect();
-  delete session;
   return 0;
 }
+
+int main( int argc, char** argv ){
+  cond::SchemaMetadataUtilities utilities;
+  return utilities.run(argc,argv);
+}
+
+
--- bin/cmscond_shuffle_iov.cpp
+++ bin/cmscond_shuffle_iov.cpp
@@ -1,17 +1,12 @@
-#include "CondCore/DBCommon/interface/CoralTransaction.h"
-#include "CondCore/DBCommon/interface/PoolTransaction.h"
-#include "CondCore/DBCommon/interface/Connection.h"
-#include "CondCore/DBCommon/interface/SessionConfiguration.h"
-#include "CondCore/DBCommon/interface/ConnectionConfiguration.h"
-#include "CondCore/DBCommon/interface/MessageLevel.h"
-#include "CondCore/DBCommon/interface/DBSession.h"
+#include "CondCore/DBCommon/interface/DbSession.h"
+#include "CondCore/DBCommon/interface/DbTransaction.h"
 #include "CondCore/DBCommon/interface/Exception.h"
 #include "CondCore/MetaDataService/interface/MetaData.h"
 #include "CondCore/IOVService/interface/IOVService.h"
 #include "CondCore/IOVService/interface/IOVEditor.h"
 #include "CondCore/Utilities/interface/CSVHeaderLineParser.h"
 #include "CondCore/Utilities/interface/CSVBlankLineParser.h"
-#include "CondCore/Utilities/interface/CommonOptions.h"
+#include "CondCore/Utilities/interface/Utilities.h"
 #include "DataFormats/Provenance/interface/EventID.h"
 #include "DataFormats/Provenance/interface/Timestamp.h"
 
@@ -21,6 +16,7 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+
 void parseInputFile(std::fstream& inputFile,
 		    std::vector< std::pair<cond::Time_t, std::string> >& newValues){
   unsigned int counter=0;
@@ -37,18 +33,18 @@ void parseInputFile(std::fstream& inputFile,
     }
     if(counter==0) {
       if(!headerParser.parse(line)) {
-	throw cms::Exception("unable to parse header: ")<<line;
+        throw cms::Exception("unable to parse header: ")<<line;
       }
       fieldNames=headerParser.result();
       unsigned int idx=0;
       for(std::vector<std::string>::iterator it=fieldNames.begin();
-	  it!=fieldNames.end(); ++it, ++idx){
-	if( *it==std::string("TILL") || *it==std::string("till") ){
-	  tillidx=idx;
-	}
-	if( *it==std::string("TOKEN") || *it==std::string("token") ){
-	  tokenidx=idx;
-	}
+          it!=fieldNames.end(); ++it, ++idx){
+        if( *it==std::string("TILL") || *it==std::string("till") ){
+          tillidx=idx;
+        }
+        if( *it==std::string("TOKEN") || *it==std::string("token") ){
+          tokenidx=idx;
+        }
       }
     }else{
       using namespace boost::spirit;
@@ -60,20 +56,20 @@ void parseInputFile(std::fstream& inputFile,
       unsigned int idx=0;
       cond::Time_t till=0;
       std::string payloadToken;
-      for(std::vector<std::string>::iterator it=result.begin(); 
-	  it!=result.end(); ++it, ++idx){
-	//std::cout<<idx<<std::endl;
-	if( idx==tillidx ){
-	  //std::cout<<"is till "<<*it<<std::endl;
-	  std::istringstream iss(*it);
-	  if((iss>>std::dec>>till).fail()){
-	    throw cms::Exception("string conversion failed");
-	  }
-	}
-	if( idx==tokenidx ){
-	  //std::cout<<"is token"<<std::endl;
-	  payloadToken=*it;
-	}
+      for(std::vector<std::string>::iterator it=result.begin();
+          it!=result.end(); ++it, ++idx){
+        //std::cout<<idx<<std::endl;
+        if( idx==tillidx ){
+          //std::cout<<"is till "<<*it<<std::endl;
+          std::istringstream iss(*it);
+          if((iss>>std::dec>>till).fail()){
+            throw cms::Exception("string conversion failed");
+          }
+        }
+        if( idx==tokenidx ){
+          //std::cout<<"is token"<<std::endl;
+          payloadToken=*it;
+        }
       }
       //std::cout<<"till "<<till<<std::endl;
       //std::cout<<"token "<<payloadToken<<std::endl;
@@ -84,149 +80,74 @@ void parseInputFile(std::fstream& inputFile,
   }
 }
 
-int main( int argc, char** argv ){
-  cond::CommonOptions myopt("cmscond_shuffle_iov","inputFile");
-  //  boost::program_options::options_description visible("Usage: cmscond_shuffle_iov [options] inputFile \n");
-  myopt.addConnect();
-  myopt.addAuthentication(true);
-  myopt.visibles().add_options()
-    ("tag,t",boost::program_options::value<std::string>(),"tag (required)")
-    ("timetype,T",boost::program_options::value<std::string>(),"timetype of the tag (optional)")
-    ("endTime,e",boost::program_options::value<cond::Time_t>(),"end time (last till) (optional)")
-    ;
-  boost::program_options::options_description invisible;
-  invisible.add_options()
-    ("inputFile",boost::program_options::value<std::string>(), "input file")
-    ;
-  myopt.description().add( myopt.visibles() );
-  myopt.description().add(invisible);
-  boost::program_options::positional_options_description posdesc;
-  posdesc.add("inputFile", -1);
+namespace cond {
+  class ShuffleIOVUtilities : public Utilities {
+    public:
+      ShuffleIOVUtilities();
+      ~ShuffleIOVUtilities();
+      int execute();
+  };
+}
 
-  boost::program_options::variables_map vm;
-  try{
-    boost::program_options::store(boost::program_options::command_line_parser(argc, argv).options(myopt.description()).positional(posdesc).run(), vm);
-    boost::program_options::notify(vm);
-  }catch(const boost::program_options::error& er) {
-    std::cerr << er.what()<<std::endl;
-    return 1;
-  }
-  if (vm.count("help")) {
-    std::cout << myopt.visibles()<<std::endl;;
-    return 0;
-  }
-  std::string connect;
-  std::string user("");
-  std::string pass("");
-  std::string authPath("");
-  std::string inputFileName;
-  std::fstream inputFile;
-  std::string tag("");
+cond::ShuffleIOVUtilities::ShuffleIOVUtilities():Utilities("cmscond_shuffle_iov","inputFile"){
+  addConnectOption();
+  addAuthenticationOptions();
+  addOption<std::string>("tag","t","tag (required)");
+  addOption<std::string>("timetype","T","timetype of the tag (optional)");
+  addOption<cond::Time_t>("endTime","e","end time (last till) (optional)");
+}
+
+cond::ShuffleIOVUtilities::~ShuffleIOVUtilities(){
+}
+
+int cond::ShuffleIOVUtilities::execute(){
+
+  std::string tag = getOptionValue("tag");
   std::string timetype("runnumber");
+  if(hasOptionValue("timetype")) timetype = getOptionValue("timetype");
   cond::Time_t  till=0;
-  bool debug=false;
+  cond::Time_t  since=0;
+  if(hasOptionValue("endTime")) till = getOptionValue("endTime");
+  bool debug= hasDebug();
+  std::string inputFileName = getOptionValue("inputFile");
   std::vector< std::pair<cond::Time_t, std::string> > newValues;
-  if( !vm.count("inputFile") ){
-    std::cerr <<"[Error] no input file given \n";
-    std::cerr<<" please do "<<argv[0]<<" --help \n";
-    return 1;
-  }else{
-    inputFileName=vm["inputFile"].as<std::string>();
-    inputFile.open(inputFileName.c_str(), std::fstream::in);
-    parseInputFile(inputFile,newValues);
-    inputFile.close();
-  }
-  if(!vm.count("connect")){
-    std::cerr <<"[Error] no connect[c] option given \n";
-    std::cerr<<" please do "<<argv[0]<<" --help \n";
-    return 1;
-  }else{
-    connect=vm["connect"].as<std::string>();
-  }
-  if(!vm.count("tag")){
-    std::cerr <<"[Error] no tag[t] option given \n";
-    std::cerr<<" please do "<<argv[0]<<" --help \n";
-    return 1;
-  }else{
-    tag=vm["tag"].as<std::string>();
-  }
-  if(vm.count("timetype")){
-    timetype = vm["timetype"].as<std::string>();
-    if(timetype=="runnumber"){
-      since=(cond::Time_t)edm::EventID::firstValidEvent().run();
-    }else if(timetype=="timestamp"){
-      since==(cond::Time_t)edm::Timestamp::beginOfTime().value();
-    }else{
-      throw cond::Exception("unsupported timetype");
-    }
-  }
-  if(vm.count("beginTime"))
-    till = (cond::Time_t) vm["beginTime"].as<cond::Time_t>();
+  std::fstream inputFile;
+  inputFile.open(inputFileName.c_str(), std::fstream::in);
+  parseInputFile(inputFile,newValues);
+  inputFile.close();
 
-  if(vm.count("debug")){
-    debug=true;
-  }
-  if(debug){
-    std::cout<<"inputFile:\t"<<inputFileName<<std::endl;
-    std::cout<<"connect:\t"<<connect<<'\n';
-    std::cout<<"tag:\t"<<tag<<'\n';
-    std::cout<<"user:\t"<<user<<'\n';
-    std::cout<<"pass:\t"<<pass<<'\n';
-    std::cout<<"authPath:\t"<<authPath<<'\n';
-  }
   std::string iovtoken;
-  cond::DBSession* session=new cond::DBSession;
-  session->configuration().connectionConfiguration()->disablePoolAutomaticCleanUp();
-  session->configuration().connectionConfiguration()->setConnectionTimeOut(0);
-
-  std::string userenv(std::string("CORAL_AUTH_USER=")+user);
-  ::putenv(const_cast<char*>(userenv.c_str()));
-  std::string passenv(std::string("CORAL_AUTH_PASSWORD=")+pass);
-  ::putenv(const_cast<char*>(passenv.c_str()));
-  if(!debug){
-    session->configuration().setMessageLevel(cond::Error);
+  cond::DbSession session= openDbSession("connect");
+  cond::IOVService iovmanager(session);
+  cond::IOVEditor* editor=iovmanager.newIOVEditor("");
+  cond::DbScopedTransaction transaction(session);
+  transaction.start(false);
+  if (timetype=="runnumber"){
+    since=(cond::Time_t)edm::EventID::firstValidEvent().run();
+    editor->create(since,cond::runnumber);
+  }else if(timetype=="timestamp"){
+    since==(cond::Time_t)edm::Timestamp::beginOfTime().value();
+    editor->create(since,cond::timestamp);
   }else{
-    session->configuration().setMessageLevel(cond::Debug);
+    throw cond::Exception("unsupported timetype");
   }
-  if(!authPath.empty()){
-    session->configuration().setAuthenticationMethod(cond::XML);
-    session->configuration().setAuthenticationPath(authPath);
-  }else{
-    session->configuration().setAuthenticationMethod( cond::Env );
+  editor->bulkInsert(newValues);
+  iovtoken=editor->token();
+  transaction.commit();
+  cond::MetaData metadata(session);
+  transaction.start(false);
+  metadata.addMapping(tag,iovtoken);
+  transaction.commit();
+  if(debug){
+    std::cout<<"source iov token "<<iovtoken<<std::endl;
   }
-  cond::Connection myconnection(connect,-1);
-  session->open();
-  try{
-    myconnection.connect(session);
-    cond::PoolTransaction& pooldb=myconnection.poolTransaction();
-    cond::IOVService iovmanager(pooldb);
-    cond::IOVEditor* editor=iovmanager.newIOVEditor("");
-    pooldb.start(false);
-    if (timetype=="runnumber"){
-      editor->create(since,cond::runnumber);
-    }else if(timetype=="timestamp"){
-      editor->create(since,cond::timestamp);
-    }else{
-      throw cond::Exception("unsupported timetype");
-    }
-    editor->bulkInsert(newValues);
-    iovtoken=editor->token();
-    pooldb.commit();
-    cond::CoralTransaction& coraldb=myconnection.coralTransaction();
-    cond::MetaData metadata(coraldb);
-    coraldb.start(false);
-    metadata.addMapping(tag,iovtoken);
-    coraldb.commit();
-    if(debug){
-      std::cout<<"source iov token "<<iovtoken<<std::endl;
-    }
-    myconnection.disconnect();
-    delete editor;
-    delete session;
-  }catch(const cond::Exception& er){
-    std::cout<<"error "<<er.what()<<std::endl;
-  }catch(const std::exception& er){
-    std::cout<<"std error "<<er.what()<<std::endl;
-  }
+  delete editor;
   return 0;
 }
+
+int main( int argc, char** argv ){
+  cond::ShuffleIOVUtilities utilities;
+  return utilities.run(argc,argv);
+}
+
+
