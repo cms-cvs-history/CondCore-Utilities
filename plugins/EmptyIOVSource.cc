48823ef8729dfd6cf765ac8c6e4af2b0b9a6c97b
cmsbuild <>
2012-11-30 15:25:06 +0000

    This commit was manufactured by cvs2git to create tag 'CondCore-Utilities- CMSSW_5_3_8_patch2'.
    Sprout from master 2012-11-30 15:24:55 UTC Giacomo Govi <sha1-c6008bb91ca6bbc745928cb6f23a999d03f434c3@cern.ch> 'added new tools for admin IOV management'
    Cherrypick from master 2010-08-06 21:06:16 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Convert BuildFiles to XML':
        plugins/BuildFile.xml
        plugins/EmptyIOVSource.cc

--- plugins/EmptyIOVSource.cc
+++ plugins/EmptyIOVSource.cc
@@ -1,14 +1,14 @@
-#include "FWCore/Sources/interface/ProducerSourceBase.h"
+#include "FWCore/Framework/interface/ConfigurableInputSource.h"
 #include <string>
 namespace cond {
-  class EmptyIOVSource : public edm::ProducerSourceBase {
+  class EmptyIOVSource : public edm::ConfigurableInputSource {
   public:
     typedef unsigned long long Time_t;
     EmptyIOVSource(edm::ParameterSet const&, edm::InputSourceDescription const&);
     ~EmptyIOVSource();
   private:
-    virtual void produce(edm::Event & e);
-    virtual bool setRunAndEventInfo(edm::EventID& id, edm::TimeValue_t& time);
+    virtual bool produce(edm::Event & e);
+    virtual void setRunAndEventInfo();
   private:
     std::string m_timeType;
     Time_t m_firstValid;
@@ -28,38 +28,42 @@ namespace cond{
   //common paras: timetype,interval
   EmptyIOVSource::EmptyIOVSource(edm::ParameterSet const& pset,
 				 edm::InputSourceDescription const& desc):
-    edm::ProducerSourceBase(pset,desc,true),
+    edm::ConfigurableInputSource(pset,desc),
     m_timeType(pset.getParameter<std::string>("timetype")),
     m_firstValid(pset.getParameter<unsigned long long>("firstValue")),
     m_lastValid(pset.getParameter<unsigned long long>("lastValue")),
     m_interval(pset.getParameter<unsigned long long>("interval")){
     m_current=m_firstValid;
+    setRunAndEventInfo(); 
   }
   EmptyIOVSource::~EmptyIOVSource() {
   }
-  void EmptyIOVSource::produce( edm::Event & ) {
-  }  
-  bool EmptyIOVSource::setRunAndEventInfo(edm::EventID& id, edm::TimeValue_t& time){
-    if(m_current<=m_lastValid){
-      if( m_timeType=="runnumber" ){
-	id = edm::EventID(m_current, id.luminosityBlock(), 1);
-      }else if( m_timeType=="timestamp" ){
-	time = m_current;
-      }else if( m_timeType=="lumiid" ){
-	edm::LuminosityBlockID l(m_current);
-        id = edm::EventID(l.run(), l.luminosityBlock(), 1);
-	//std::cout<<"run "<<l.run()<<std::endl;
-	//std::cout<<"luminosityBlock "<<l.luminosityBlock()<<std::endl;
-      }else{
-	throw edm::Exception(edm::errors::Configuration, std::string("EmptyIOVSource::setRunAndEventInfo: ")+m_timeType+std::string("is not one of the supported types: runnumber,timestamp,lumiid") );
-      }
-    }
+  bool EmptyIOVSource::produce( edm::Event & e ) {
     bool ok = !(m_lastValid<m_current);
     m_current += m_interval;
     return ok;
+  }  
+  void EmptyIOVSource::setRunAndEventInfo(){
+    if(m_current<=m_lastValid){
+      if( m_timeType=="runnumber" ){
+	setRunNumber(m_current);
+      }else if( m_timeType=="timestamp" ){
+	setTime(m_current);
+      }else if( m_timeType=="lumiid" ){
+	edm::LuminosityBlockID l(m_current);
+	setRunNumber(l.run());
+	//std::cout<<"run "<<l.run()<<std::endl;
+	//std::cout<<"luminosityBlock "<<l.luminosityBlock()<<std::endl;
+	setLuminosityBlockNumber_t(l.luminosityBlock());
+      }else{
+	throw edm::Exception(edm::errors::Configuration, std::string("EmptyIOVSource::setRunAndEventInfo: ")+m_timeType+std::string("is not one of the supported types: runnumber,timestamp,lumiid") );
+      }
+      setEventNumber(1);
+    }
   }
 }//ns cond
 
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/InputSourceMacros.h"
 using cond::EmptyIOVSource;
 
